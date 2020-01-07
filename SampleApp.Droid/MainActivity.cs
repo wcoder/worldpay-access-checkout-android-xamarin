9fb867793a7e0f27238fb1b8a6f1c5bb34f9dfcc
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-01-07 21:01:20 +0300

    Add sample.

--- SampleApp.Droid/MainActivity.cs
+++ SampleApp.Droid/MainActivity.cs
@@ -1,59 +1,84 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
 using Android.Support.V7.App;
-using Android.Views;
 using Android.Widget;
+
+using Com.Worldpay.Access.Checkout;
+using Com.Worldpay.Access.Checkout.Api;
+using Com.Worldpay.Access.Checkout.Views;
 
 namespace SampleApp.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private PANLayout _panView;
+        private CardCVVText _cardCVVText;
+        private CardExpiryTextLayout _cardExpiryText;
+        private Button _submit;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            Do();
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        private void Do()
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+            _panView = FindViewById<PANLayout>(Resource.Id.panView);
+            _cardCVVText = FindViewById<CardCVVText>(Resource.Id.cardCVVText);
+            _cardExpiryText = FindViewById<CardExpiryTextLayout>(Resource.Id.cardExpiryText);
+            _submit = FindViewById<Button>(Resource.Id.submit_button);
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            var sessionResponseListener = new YourSessionResponseListener();
+
+            var accessCheckoutClient = AccessCheckoutClient.Init(
+                "https://try.access.worldpay.com/", // Base API URL
+                "",// Your merchant ID
+                sessionResponseListener, // SessionResponseListener
+                this, // Context
+                this // LifecycleOwner
+                );
+
+            _submit.Click += (s, e) =>
             {
-                return true;
-            }
+                var pan = _panView.InsertedText;
+                int month = _cardExpiryText.Month;
+                int year = _cardExpiryText.Year;
+                var cvv = _cardCVVText.InsertedText;
 
-            return base.OnOptionsItemSelected(item);
+                accessCheckoutClient.GenerateSessionState(pan, month, year, cvv);
+            };
+        }
+    }
+
+    /// <summary>
+    /// The <see cref="ISessionResponseListener"/> will receive an update once the session state has been generated.
+    /// The listener will also be notified of any errors that could have occurred during the request.
+    /// </summary>
+    class YourSessionResponseListener : Java.Lang.Object, ISessionResponseListener
+    {
+        /// <summary>
+        /// Method for being notified when the request for the session state starts.
+        /// </summary>
+        public void OnRequestStarted()
+        {
+
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
+        /// <summary>
+        /// Method for being notified when the session state is available, or an error has occurred when trying to generate one
+        /// </summary>
+        /// <param name="sessionState">(Optional) when the session has been created successfully</param>
+        /// <param name="error">
+        ///     (Optional) a subclass of <see cref="AccessCheckoutException"/>
+        ///     when there has been an error generating the session state
+        /// </param>
+        public void OnRequestFinished(string sessionState, AccessCheckoutException error)
         {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
